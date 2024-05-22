458c10936926b94e344fda9f8c1b945c582e754d
MaxChe <factor.x2016@yandex.ru>
2024-05-23 00:04:34 +0300

    Change Test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Netology");
+        int priceTicket = 19800; // стоимость билета
+        int priceMile = 33; // стоимость мили
+
+        int miles; // переменная
+        if (priceTicket < 0) {
+            System.out.println("Не может быть отрицательного значения");
+        } else {
+            miles = priceTicket / priceMile; // кол-во миль
+            System.out.println(miles); // результат
+        }
+
     }
 }
