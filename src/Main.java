03223b70c4711b2a07c835235ad6cea62e2bb6f7
MaxChe <factor.x2016@yandex.ru>
2024-05-24 23:10:18 +0300

    Add file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int priceTicket = 19800; // стоимость билета
-        int priceMile = 33; // стоимость мили
+        BonusMilesService service = new BonusMilesService();
+        int price = 10000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles); // результат
 
-        int miles; // переменная
-        if (priceTicket < 0) {
-            System.out.println("Не может быть отрицательного значения");
-        } else {
-            miles = priceTicket / priceMile; // кол-во миль
-            System.out.println(miles); // результат
-        }
 
     }
 }
